2c5465c3cbcef2ab3834ee782f40434a9c1126d1
kartiksingla <singla.kartik1@gmail.com>
2018-06-21 12:56:33 +0530

    Adding rest url's file and adding it to main application

--- src/main/java/com/analysis/shared/app/ApplicationConfiguration.java
+++ src/main/java/com/analysis/shared/app/ApplicationConfiguration.java
@@ -1,12 +1,20 @@
 package com.analysis.shared.app;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.analysis.shared.app")
+@PropertySource({ "classpath:application.properties", "classpath:rest-api-urls.properties" })
 public class ApplicationConfiguration {
-	
+
+	@Bean
+	static PropertySourcesPlaceholderConfigurer propertyPlaceHolderConfigurer() {
+		return new PropertySourcesPlaceholderConfigurer();
+	}
 }
